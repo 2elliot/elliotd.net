95fd373990c31aec772125d5649834a45a1acfef
Elliot <ejdalessa@gmail.com>
2025-08-18 08:41:09 -0700

    Built

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // If you deploy to GitHub Pages at a subpath, set base: '/your-repo-name/'
   base: '/',
 })
